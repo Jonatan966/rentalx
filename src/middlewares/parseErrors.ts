af94c46cbdb6563a43f7fb653a6730441440fea1
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-10 17:04:37 +0000

    feat: create error parser

---
+++ src/middlewares/parseErrors.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '../errors/AppError';
+
+export function parseErrors(
+  error: Error,
+  _: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${error.message}`,
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import { parseErrors } from './middlewares/parseErrors';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -13,5 +15,6 @@ const app = express();
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
+app.use(parseErrors);
 
 app.listen(3333, () => console.log('working'));
