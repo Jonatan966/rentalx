20ea1398f1497cb1ff2f4b2d740accbb2418077c
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-07 20:12:37 +0000

    feat: add car creation route

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../shared/errors/AppError';
+import { ICreateCarDTO } from '../../dtos/ICreateCarDTO';
+import { Car } from '../../infra/typeorm/entities/Car';
+import { ICarsRepository } from '../../repositories/ICarsRepository';
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists');
+    }
+
+    const createdCar = await this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    return createdCar;
+  }
+}
+
+export { CreateCarUseCase };
