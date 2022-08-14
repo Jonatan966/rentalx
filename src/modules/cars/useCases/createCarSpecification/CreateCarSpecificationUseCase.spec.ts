58c02e819ed66ea80032cb6a95f0de4e69c4ff53
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-14 14:06:18 +0000

    feat: create use case to update car specifications

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,61 @@
+import { AppError } from '../../../../shared/errors/AppError';
+import { CarsRepositoryInMemory } from '../../repositories/in-memory/CarsRepositoryInMemory';
+import { SpecificationsRepositoryInMemory } from '../../repositories/in-memory/SpecificationsRepositoryInMemory';
+import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
+
+describe('Create Car Specification', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
+    );
+  });
+
+  it('should be able to add a new specification to the car', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Fake Car',
+      description: 'Fake description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'brand',
+      category_id: 'category',
+    });
+
+    const specification = await specificationsRepositoryInMemory.create({
+      description: 'test',
+      name: 'test',
+    });
+
+    const specifications_id = [specification.id];
+
+    const createdCarSpecification = await createCarSpecificationUseCase.execute(
+      {
+        car_id: car.id,
+        specifications_id,
+      }
+    );
+
+    expect(createdCarSpecification).toHaveProperty('specifications', [
+      specification,
+    ]);
+  });
+
+  it('should not be able to add a new specification to a non existent car', () => {
+    expect(async () => {
+      const car_id = '1234';
+      const specifications_id = ['54321'];
+
+      await createCarSpecificationUseCase.execute({
+        car_id,
+        specifications_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
