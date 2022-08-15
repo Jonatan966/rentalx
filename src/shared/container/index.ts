c311eba85ee146d7599ea9184bc1eb220b83503e
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-15 19:39:24 +0000

    feat: create rentals repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,8 @@ import { ICarImagesRepository } from '../../modules/cars/repositories/ICarImages
 import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { RentalsRepository } from '../../modules/rentals/infra/typeorm/repositories/RentalsRepository';
+import { IRentalsRepository } from '../../modules/rentals/repositories/IRentalsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -33,4 +35,9 @@ container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
 container.registerSingleton<ICarImagesRepository>(
   'CarImagesRepository',
   CarImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository
 );
