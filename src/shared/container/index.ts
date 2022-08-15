6e01f1f652e231eccc2bddc98dfe0d3bc58196eb
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-15 19:38:25 +0000

    feat: create dayjs date provider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import './providers';
+
 import { UsersRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { CarImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarImagesRepository';
