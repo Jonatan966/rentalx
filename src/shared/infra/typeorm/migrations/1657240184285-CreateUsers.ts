54361a59e7aa2e4eb7d3f3bfcef812735ffd2017
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-15 19:36:49 +0000

    fix: put primary key into users table

--- src/shared/infra/typeorm/migrations/1657240184285-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1657240184285-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1657240184285 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
