840d96265e0df95e4c263a5eb177bdfe1c2b9bd9
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-08-19 10:14:45 -0300

    feat: add tests in user creation

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { AppError } from '../../../../shared/errors/AppError';
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Create User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it('should be able to create a new user', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'fake user',
+      email: 'user@email.com',
+      password: 'the-password',
+      driver_license: '1234-ABCD',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a user with same e-mail', async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: 'fake user 1',
+        email: 'user@email.com',
+        password: 'the-password',
+        driver_license: '1234-ABCD',
+      });
+
+      await createUserUseCase.execute({
+        name: 'fake user 2',
+        email: 'user@email.com',
+        password: 'the-password-2',
+        driver_license: 'ABCD-1234',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
