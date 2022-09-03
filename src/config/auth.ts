d80c0778b89bee608cdf540a4e0887775664088e
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-09-03 10:23:58 -0300

    refact: transfer auth keys to separate file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '85de28c898163bc78af163219b4424fc',
+  secret_refresh_token: '862772db1142a04430e06acea20e34be',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import auth from '../../../../config/auth';
 import { UsersRepository } from '../../../../modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { AppError } from '../../../errors/AppError';
 
@@ -22,10 +23,7 @@ export async function ensureAuthenticated(
   const parsedToken = authHeader.replace('Bearer ', '');
 
   try {
-    const { sub: user_id } = verify(
-      parsedToken,
-      '85de28c898163bc78af163219b4424fc'
-    ) as IPayload;
+    const { sub: user_id } = verify(parsedToken, auth.secret_token) as IPayload;
 
     const usersRepository = new UsersRepository();
 
