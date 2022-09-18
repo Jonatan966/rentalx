1422979188e39da5cdc564185415132968b73296
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-09-17 21:10:33 -0300

    feat: serve static images path

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerConfig from '../../../config/swagger';
+import upload from '../../../config/upload';
 import swaggerFile from '../../../swagger.json';
 import createConnection from '../typeorm';
 import { parseErrors } from './middlewares/parseErrors';
@@ -20,6 +21,10 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerFile, swaggerConfig)
 );
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+
 app.use(router);
 app.use(parseErrors);
 app.use(express.static(path.resolve(__dirname, '..', '..', '..', 'public')));
