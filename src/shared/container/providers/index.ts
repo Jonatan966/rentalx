5aac6b3cff2c471fc3465c04996e5cd17ad5b84a
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-09-17 21:13:13 -0300

    refact: adjust containers import

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,16 +1,3 @@
-import { container } from 'tsyringe';
-
-import { IDateProvider } from './DateProvider/IDateProvider';
-import { DayJsDateProvider } from './DateProvider/implementations/DayJsDateProvider';
-import { IMailProvider } from './MailProvider/IMailProvider';
-import { EtherialMailProvider } from './MailProvider/implementations/EtherialMailProvider';
-
-container.registerSingleton<IDateProvider>(
-  'DayJsDateProvider',
-  DayJsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  'EtherialMailProvider',
-  new EtherialMailProvider()
-);
+import './DateProvider';
+import './MailProvider';
+import './StorageProvider';
