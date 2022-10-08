9b93e594884795533ec6926eaf9b179fe86c70db
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-10-08 20:23:14 -0300

    feat: config sentry

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
@@ -16,10 +18,26 @@ import '../../container';
 
 createConnection();
 const app = express();
+const isProduction = process.env.NODE_ENV === 'prod';
 
 app.use(rateLimiter);
 
+if (isProduction) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use(express.json());
+
 app.use(
   '/api-docs',
   swaggerUi.serve,
@@ -30,6 +48,11 @@ app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
+
+if (isProduction) {
+  app.use(Sentry.Handlers.errorHandler());
+}
+
 app.use(parseErrors);
 app.use(express.static(path.resolve(__dirname, '..', '..', '..', 'public')));
 
